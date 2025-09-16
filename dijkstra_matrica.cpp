edf0edf21030a396b7cb7afe51e5a0b3982d3336
slavevski18 <slavevskimatej@gmail.com>
2025-09-16 21:13:27 +0200

    matrica_dijkstra

---
+++ dijkstra_matrica.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+using namespace std;
+
+int main() {
+    int V; 
+    cout << "Vnesi broj na teminja"<<endl;
+    cin >> V;
+
+    vector<vector<int>> graph(V, vector<int>(V));
+    cout << "Vnesi ja matricata"<<endl;
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            cin >> graph[i][j];
+            if (graph[i][j] == 0 && i != j) {
+                graph[i][j] = numeric_limits<int>::max(); 
+            }
+        }
+    }
+
+    int matej;
+    cout << "Od koe teme sakas da najdes najkratok pat? "<<endl;
+    cin >> matej;
+
+    vector<int> dist(V, numeric_limits<int>::max());
+    vector<bool> visited(V, false);
+    dist[matej] = 0;
+
+    for (int count = 0; count < V - 1; count++) {
+        int u = -1;
+        for (int i = 0; i < V; i++) {
+            if (!visited[i] && (u == -1 || dist[i] < dist[u])) {
+                u = i;
+            }
+        }
+
+        visited[u] = true;
+
+        for (int v = 0; v < V; v++) {
+            if (graph[u][v] != numeric_limits<int>::max() && !visited[v]) {
+                if (dist[u] + graph[u][v] < dist[v]) {
+                    dist[v] = dist[u] + graph[u][v];
+                }
+            }
+        }
+    }
+
+    cout << endl << "najkratkite rastojanija od temeto " << matej << " se:" << endl;
+    for (int i = 0; i < V; i++) {
+        cout << endl << "do " << i << ": ";
+        if (dist[i] == numeric_limits<int>::max()) {
+            cout << "INF" << endl;
+        } else {
+            cout << dist[i] << endl;
+        }
+    }
+
+    return 0;
+}
