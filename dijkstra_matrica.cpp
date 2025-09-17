4fd6df1199bc73669378b2fbf8b9aa53a9456c67
slavevski18 <slavevskimatej@gmail.com>
2025-09-17 11:18:11 +0200

    bellmanford

--- dijkstra_matrica.cpp
+++ dijkstra_matrica.cpp
@@ -11,10 +11,10 @@ int main() {
     vector<vector<int>> graph(V, vector<int>(V));
     cout << "Vnesi ja matricata"<<endl;
     for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
+        for (int j = 0; j < V; j++) { /*prave gu matricata*/
             cin >> graph[i][j];
             if (graph[i][j] == 0 && i != j) {
-                graph[i][j] = numeric_limits<int>::max(); 
+                graph[i][j] = numeric_limits<int>::max(); /*ako e 0, znaci nema rebro i ga zamenuva so infinity*/
             }
         }
     }
@@ -23,24 +23,25 @@ int main() {
     cout << "Od koe teme sakas da najdes najkratok pat? "<<endl;
     cin >> matej;
 
-    vector<int> dist(V, numeric_limits<int>::max());
-    vector<bool> visited(V, false);
+    vector<int> dist(V, numeric_limits<int>::max());  /* u dist ga cuva najmalata distanca od matej, prvicno site sa infinity, 
+    a distancata na matej e 0 poso od pocetoko sekat e 0*/
+    vector<bool> visited(V, false); /*visited gi cuva do koi nodovi e najdena najkratkata distanca*/
     dist[matej] = 0;
 
     for (int count = 0; count < V - 1; count++) {
         int u = -1;
         for (int i = 0; i < V; i++) {
-            if (!visited[i] && (u == -1 || dist[i] < dist[u])) {
+            if (!visited[i] && (u == -1 || dist[i] < dist[u])) { /*naoga gi neposetenite teminja so najmala distanca*/
                 u = i;
             }
         }
 
-        visited[u] = true;
+        visited[u] = true; /* znaemo gu najmalata distanca do toa teme*/
 
-        for (int v = 0; v < V; v++) {
-            if (graph[u][v] != numeric_limits<int>::max() && !visited[v]) {
-                if (dist[u] + graph[u][v] < dist[v]) {
-                    dist[v] = dist[u] + graph[u][v];
+        for (int v = 0; v < V; v++) {                                       /*For each neighbor v of u*/
+            if (graph[u][v] != numeric_limits<int>::max() && !visited[v]) { /*If there is an edge (graph[u][v] != ∞) and v is not visited*/
+                if (dist[u] + graph[u][v] < dist[v]) {                      /*Check if going through u gives a shorter path to v*/
+                    dist[v] = dist[u] + graph[u][v];                        /*If yes, update dist[v]*/
                 }
             }
         }
